29fca1b24da8d6573e5e39f319002717d01dace1
Chase Moskal <chasemoskal@gmail.com>
2019-06-02 10:57:11 -0700

    sort blog posts by date

--- tooling/blog-toolbox.js
+++ tooling/blog-toolbox.js
@@ -5,11 +5,31 @@ const grayMatter = require("gray-matter")
 
 const blogPostFileNameRegex = /^(\d{4}-\d{2}-\d{2})[-=:](.*)\.md$/i
 
+const parseDate = date => {
+	const [rawYear, rawMonth, rawDay] = date.split("-")
+	return {
+		year: parseInt(rawYear),
+		month: parseInt(rawMonth),
+		day: parseInt(rawDay)
+	}
+}
+
 async function readPosts(postsDir) {
 	const posts = await Promise.all(
 		(await fs.readdir(postsDir)).map(name => readPost(postsDir, name))
 	)
-	return posts.filter(post => !!post)
+	return posts
+		.filter(post => !!post)
+		.sort((postA, postB) => {
+			const dateA = parseDate(postA.date)
+			const dateB = parseDate(postB.date)
+			if (dateA.year > dateB.year && dateA.month > dateB.month && dateA.day > dateB.day)
+				return -1
+			else if (dateA.year === dateB.year && dateA.month === dateB.month && dateA.day === dateB.day)
+				return 0
+			else
+				return 1
+		})
 }
 
 async function makeBlogIndex({blogIndexTemplate, posts, distDir, makePostPath}) {
