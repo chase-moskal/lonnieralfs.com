e16ef1cbf1eee6df9711ecf330f1b9aafd4b1dd8
Chase Moskal <chasemoskal@gmail.com>
2019-08-01 15:35:37 -0700

    attempt to fix animation hiccups, still occurs

--- source/catalog-grid.js
+++ source/catalog-grid.js
@@ -1,22 +1,16 @@
 
-const sleep = async(time) => new Promise(resolve => setTimeout(resolve, time))
+const sleep = async(time) => new Promise(
+	resolve => setTimeout(() => requestAnimationFrame(resolve), time)
+)
 
 export class CatalogGrid {
-	_animTime = null
-	_reupdateCue = false
-	_catalogElement = null
 
 	constructor({catalogElement, animTime}) {
 		this._lastLabel = ""
 		this._inProgress = false
 		this._animTime = animTime
+		this._animInProgress = false
 		this._catalogElement = catalogElement
-
-		// initial update
-		this._handleChange()
-
-		// also update whenever fragment identifier changes
-		window.addEventListener("hashchange", () => this._handleChange())
 
 		// install click-to-close functionality
 		const links = Array.from(catalogElement.querySelectorAll("a"))
@@ -28,11 +22,31 @@ export class CatalogGrid {
 				return false
 			}
 		}
+
+		// initialize animation properties
+		for (const div of this.labeledDivs) {
+			div.hidden = true
+			div.setAttribute("data-visible", "false")
+		}
+
+		// initial update
+		this._handleChange()
+
+		// also update whenever fragment identifier changes
+		window.addEventListener("hashchange", () => this._handleChange())
 	}
 
 	get label() {
 		const {hash: rawHash} = location
 		return rawHash[0] === "#" ? rawHash.slice(1) : rawHash
+	}
+
+	get links() {
+		return Array.from(this._catalogElement.querySelectorAll("a"))
+	}
+
+	get labeledDivs() {
+		return Array.from(this._catalogElement.querySelectorAll("[data-label]"))
 	}
 
 	_handleChange() {
@@ -43,31 +57,22 @@ export class CatalogGrid {
 		}
 	}
 
-	_animInProgress = false
-
 	async _sweetAnimLoop(label) {
-		const {
-			_animTime: animTime,
-			_catalogElement: catalogElement
-		} = this
+		const animTime = this._animTime
 
 		// update [data-active] on all links
-		const links = Array.from(catalogElement.querySelectorAll("a"))
-		for (const link of links) {
+		for (const link of this.links) {
 			const [,linkLabel] = /#(.*)$/i.exec(link.href) || [null, null]
 			if (linkLabel === label) link.setAttribute("data-active", "data-active")
 			else link.removeAttribute("data-active")
 		}
 
+		// perform animations and set [hidden] on divs
 		if (!this._animInProgress) {
 			this._animInProgress = true
-
-			// keep all animation promises so we know when they're all done
 			const operations = []
 
-			// perform animations and set [hidden] on divs
-			const labeledDivs = Array.from(catalogElement.querySelectorAll("[data-label]"))
-			for (const div of labeledDivs) {
+			for (const div of this.labeledDivs) {
 				const dataLabel = div.getAttribute("data-label")
 				const matching = dataLabel === label
 				const previousHidden = div.hidden
@@ -77,14 +82,12 @@ export class CatalogGrid {
 				if (!previousHidden && nowHidden) {
 					div.hidden = false
 					operations.push(
-						sleep(0).then(() => {
-							div.setAttribute("animate-visible", "false")
-						})
+						sleep(0)
+							.then(() => div.setAttribute("animate-visible", "false"))
 					)
 					operations.push(
-						sleep(animTime).then(() => {
-							div.hidden = true
-						})
+						sleep(animTime)
+							.then(() => div.hidden = true)
 					)
 				}
 
@@ -92,12 +95,9 @@ export class CatalogGrid {
 				else if (previousHidden && !nowHidden) {
 					div.hidden = false
 					operations.push(
-						sleep(0).then(() => {
-							div.setAttribute("animate-visible", "true")
-						})
-					)
-					operations.push(
-						sleep(animTime)
+						sleep(0)
+							.then(() => div.setAttribute("animate-visible", "true"))
+							.then(() => sleep(animTime))
 					)
 				}
 			}
